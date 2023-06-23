cf905e1015665a67737820bac01e380f82603e36
wonieeVicky <hwfongfing@gmail.com>
2023-06-23 23:44:14 +0900

    feat: SSG 에서 특정 params에 대해 static page로 생성하도록 generateStaticParams 함수 적용

--- nextjs-note/src/app/products/[slug]/page.tsx
+++ nextjs-note/src/app/products/[slug]/page.tsx
@@ -7,3 +7,11 @@
 export default function PantsPage({ params }: Props) {
   return <div>{params.slug} 제품 설명 페이지</div>;
 }
+
+// 미리 페이지를 만들어두고 싶은 경우 generateStaticParams 함수를 사용한다.
+export function generateStaticParams() {
+  const products = ['pants', 'skirt'];
+  return products.map((product) => ({
+    slug: product
+  }));
+}
