9fa9dd9f9ebac205a82c9054a9f114885069deca
wonieeVicky <hwfongfing@gmail.com>
2023-06-25 23:44:44 +0900

    feat: 페이지 링크 연결(a tag > Link tag)

--- nextjs-note/src/app/layout.tsx
+++ nextjs-note/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import './globals.css';
 import styles from './layout.module.css';
 import { Inter } from 'next/font/google';
@@ -17,8 +18,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <header className={styles.header}>
           <h1>Demo Note</h1>
           <nav className={styles.nav}>
-            <a href="">Contact</a>
-            <a href="">About</a>
+            <Link href="/products">Products</Link>
+            <Link href="/contact">Contact</Link>
+            <Link href="/about">About</Link>
           </nav>
         </header>
         {children}
--- nextjs-note/src/app/products/layout.tsx
+++ nextjs-note/src/app/products/layout.tsx
@@ -1,12 +1,13 @@
 ﻿import React from 'react';
 import styles from './layout.module.css';
+import Link from 'next/link';
 
 export default function ProductsLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <nav className={styles.nav}>
-        <a href="">여성옷</a>
-        <a href="">남성옷</a>
+        <Link href="/products/women">여성옷</Link>
+        <Link href="/products/man">남성옷</Link>
       </nav>
       <section className={styles.product}>{children}</section>
     </>
