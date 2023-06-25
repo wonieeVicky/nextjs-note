78173653d7b370aa93f3d31e99c3a2f8524416cf
wonieeVicky <hwfongfing@gmail.com>
2023-06-25 23:53:34 +0900

    feat: products 내부 카테고리별 Link 추가

--- nextjs-note/src/app/products/page.tsx
+++ nextjs-note/src/app/products/page.tsx
@@ -1,3 +1,19 @@
-﻿export default function ProductsPage() {
-  return <div>제품 소개 페이지</div>;
+﻿import Link from 'next/link';
+import styles from './layout.module.css';
+
+const products = ['shirt', 'pants', 'skirt', 'shoes'];
+
+export default function ProductsPage() {
+  return (
+    <>
+      <h1>제품 소개 페이지</h1>
+      <ul className={styles.productUl}>
+        {products.map((product) => (
+          <li key={product}>
+            <Link href={`products/${product}`}>{product}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
