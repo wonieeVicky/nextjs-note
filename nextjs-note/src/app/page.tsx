d215f6c2b6a5a345a2984a54b8e17a3f118a6cba
wonieeVicky <hwfongfing@gmail.com>
2023-06-28 23:53:56 +0900

    test: server-component

--- nextjs-note/src/app/page.tsx
+++ nextjs-note/src/app/page.tsx
@@ -1,6 +1,10 @@
 import Image from 'next/image';
 import styles from './page.module.css';
+import os from 'os'; // 노드 APIs
 
 export default function Home() {
+  console.log('hi!?');
+  console.log(os.hostname());
+
   return <h1>홈페이지!</h1>;
 }
