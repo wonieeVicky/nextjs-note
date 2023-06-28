7232bcb3103d211c84ce53bf2400b7ba5ecb7568
wonieeVicky <hwfongfing@gmail.com>
2023-06-28 23:15:44 +0900

    feat: global layout 및 특정 라우트 하위에 각 metaData 적용

--- nextjs-note/src/app/products/layout.tsx
+++ nextjs-note/src/app/products/layout.tsx
@@ -1,6 +1,12 @@
 ﻿import React from 'react';
 import styles from './layout.module.css';
 import Link from 'next/link';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '멋진 비키 사이트 | 제품 확인',
+  description: '멋진 제품을 확인해보세요.'
+};
 
 export default function ProductsLayout({ children }: { children: React.ReactNode }) {
   return (
