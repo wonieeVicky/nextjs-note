3da3f3baf760e4e12a61cca009264725ff2264cb
wonieeVicky <hwfongfing@gmail.com>
2023-06-29 22:08:02 +0900

    feat: client component 구현 및 import

--- nextjs-note/src/app/page.tsx
+++ nextjs-note/src/app/page.tsx
@@ -1,10 +1,16 @@
 import Image from 'next/image';
 import styles from './page.module.css';
-import os from 'os'; // 노드 APIs
+import os from 'os';
+import Counter from '@/components/Counter';
 
 export default function Home() {
-  console.log('hi!?');
+  console.log('hi! - server component');
   console.log(os.hostname());
 
-  return <h1>홈페이지!</h1>;
+  return (
+    <>
+      <h1>홈페이지!</h1>
+      <Counter />
+    </>
+  );
 }
