a590d7d5d4d0b8bd45684415cbb9bc987dd980c0
wonieeVicky <hwfongfing@gmail.com>
2023-07-01 00:19:59 +0900

    feat: getProducts 내 비동기 로직 추가(products.json readFile), products/page 내 반영

--- nextjs-note/src/app/products/page.tsx
+++ nextjs-note/src/app/products/page.tsx
@@ -1,18 +1,15 @@
-﻿import Link from 'next/link';
-import styles from './layout.module.css';
-import { getProducts } from '@/service/products';
+﻿import { getProducts } from '@/service/products';
+import Link from 'next/link';
 
-export default function ProductsPage() {
-  // 서버 파일(데이터베이스)에 있는 제품의 리스트를 읽어와서 그걸 보여준다.
-  const products = getProducts();
-
+export default async function ProductsPage() {
+  const products = await getProducts();
   return (
     <>
-      <h1>제품 소개 페이지</h1>
-      <ul className={styles.productUl}>
-        {products.map((product) => (
-          <li key={product}>
-            <Link href={`products/${product}`}>{product}</Link>
+      <h1>제품 소개 페이지!</h1>
+      <ul>
+        {products.map(({ id, name }, index) => (
+          <li key={index}>
+            <Link href={`/products/${id}`}>{name}</Link>
           </li>
         ))}
       </ul>
--- nextjs-note/src/service/products.ts
+++ nextjs-note/src/service/products.ts
@@ -1,5 +1,16 @@
-﻿export function getProducts() {
-  return ['shirt', 'pants', 'skirt', 'shoes', 'dress'];
+﻿import path from 'path';
+import { promises as fs } from 'fs';
+
+export type Product = {
+  id: string;
+  name: string;
+  price: number;
+};
+
+export async function getProducts(): Promise<Product[]> {
+  const filePath = path.join(process.cwd(), 'data', 'products.json');
+  const data = await fs.readFile(filePath, 'utf-8');
+  return JSON.parse(data);
 }
 
 export function getProduct(id: string) {
