bfa8262f09ad0641bffffe08b9d4dd142612cf3d
wonieeVicky <hwfongfing@gmail.com>
2023-07-01 23:53:12 +0900

    feat: fetch를 사용한 SSG 렌더링 및 fetch 내 revalidate 조건 처리 추가

--- nextjs-note/src/app/products/page.tsx
+++ nextjs-note/src/app/products/page.tsx
@@ -1,10 +1,17 @@
 ﻿import { getProducts } from '@/service/products';
 import Link from 'next/link';
+import styles from './page.module.css';
 
-export const revalidate = 3;
+// export const revalidate = 3;
 
 export default async function ProductsPage() {
   const products = await getProducts();
+  const res = await fetch('http://meowfacts.herokuapp.com/', {
+    next: { revalidate: 3 }
+  });
+  const data = await res.json();
+  const factText = data.data[0];
+
   return (
     <>
       <h1>제품 소개 페이지!</h1>
@@ -15,6 +22,7 @@ export default async function ProductsPage() {
           </li>
         ))}
       </ul>
+      <article className={styles.article}>{factText}</article>
     </>
   );
 }
