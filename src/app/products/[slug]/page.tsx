8100c55a05d12b405089f18e62fde5bedfb92103
wonieeVicky <hwfongfing@gmail.com>
2023-07-05 23:46:47 +0900

    fix: slug page 내 이미지 호출 방식 변경

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,9 +1,6 @@
 ﻿import { getProduct, getProducts } from '@/service/products';
 import { notFound } from 'next/navigation';
-import Image, { StaticImageData } from 'next/image';
-import jeansImage from '../../../../public/images/jeans.jpg';
-import shoesImage from '../../../../public/images/shoes.jpg';
-import tshirtImage from '../../../../public/images/tshirt.jpg';
+import Image from 'next/image';
 
 export const revalidate = 3;
 
@@ -26,17 +23,11 @@ export default async function ProductPage({ params: { slug } }: Props) {
     notFound();
   }
 
-  const mapSrcToImage: Record<string, StaticImageData> = {
-    jeans: jeansImage,
-    shoes: shoesImage,
-    tshirt: tshirtImage
-  };
-
   // 서버 파일에 있는 데이터 중 해당 제품의 정보를 찾아와 그 정보를 보여준다.
   return (
     <>
       <h1>{product.name} 제품 설명 페이지</h1>
-      <Image src={mapSrcToImage[slug]} alt={`${slug}Image`} width={500} />
+      <Image src={`/images/${product.image}`} alt={`${product.name} Image`} width={300} height={300} />
     </>
   );
 }
