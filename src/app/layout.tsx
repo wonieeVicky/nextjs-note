006a82f085921e9656b41aa56d2c7b56925f922e
wonieeVicky <hwfongfing@gmail.com>
2023-07-06 23:33:03 +0900

    feat: @next/font로 open sans, nanum gothic 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 import Link from 'next/link';
 import './globals.css';
 import styles from './layout.module.css';
-import { Inter } from 'next/font/google';
+import { Open_Sans, Nanum_Gothic } from 'next/font/google';
 import { Metadata } from 'next';
 
-const inter = Inter({ subsets: ['latin'] });
+const sans = Open_Sans({ subsets: ['latin'] });
+const gothic = Nanum_Gothic({ weight: '700', subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: '멋진 비키 사이트',
@@ -13,10 +14,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
+    <html lang="en" className={sans.className}>
+      <body>
         <header className={styles.header}>
-          <h1>Demo Note</h1>
+          <h1 className={gothic.className}>Demo Note</h1>
           <nav className={styles.nav}>
             <Link href="/products">Products</Link>
             <Link href="/contact">Contact</Link>
