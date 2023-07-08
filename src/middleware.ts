1c9c04f068bc718c1c7ae246827f977638bb3002
wonieeVicky <hwfongfing@gmail.com>
2023-07-08 23:29:20 +0900

    feat: 적용 products 하위 모든 경로에 대한 middleware 적용

---
+++ src/middleware.ts
@@ -0,0 +1,13 @@
+﻿import { NextRequest, NextResponse } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  console.log('미들웨어 실행 중!!');
+  if (request.nextUrl.pathname.startsWith('/products/1004')) {
+    console.log('미들웨어 - 경로 리다이렉팅함!!');
+    return NextResponse.redirect(new URL('products', request.url));
+  }
+}
+
+export const config = {
+  matcher: ['/products/:path*']
+};
